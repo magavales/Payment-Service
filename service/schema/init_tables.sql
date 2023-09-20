45f40c327e3774e9fd027efc89813fa2b99b68e0
magavales <filippovdenis-2002@yandex.ru>
2023-09-20 15:19:25 +0300

    Rework schema

--- service/schema/init_tables.sql
+++ service/schema/init_tables.sql
@@ -1,5 +1,12 @@
 CREATE TABLE data_balance (
     id bigserial primary key not null,
-    user_id varchar(32) not null,
+    user_id varchar(64) not null,
     balance bigint not null
+)
+
+CREATE TABLE history_table (
+    user_id varchar(64) not null,
+    to_id varchar(64) not null,
+    operation varchar(16) not null,
+    amount bigint not null
 )
